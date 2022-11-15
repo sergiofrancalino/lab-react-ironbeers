90e6c75714d60305d330ef854aab51a48aa0a7f2
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-11-14 22:57:48 -0300

    Interation 4

--- src/pages/AllBeers.js
+++ src/pages/AllBeers.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import AllBeers from "./AllBeers";
+
 
 function AllBeers() {
   const [beers, setBeers] = useState([]);
